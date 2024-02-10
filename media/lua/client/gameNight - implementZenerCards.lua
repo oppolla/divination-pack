95a955bd98b4638bfec556fe89352426278e4843
Andy <aeudet@gmail.com>
2024-02-09 23:48:15 -0500

    many changes

---
+++ media/lua/client/gameNight - implementZenerCards.lua
@@ -0,0 +1,32 @@
+local applyItemDetails = require "gameNight - applyItemDetails"
+
+local zenerCards = {
+	"Circle",
+	"Circle",
+	"Circle",
+	"Circle",
+	"Circle",
+	"Square",
+	"Square",
+	"Square",
+	"Square",
+	"Square",
+	"Star",
+	"Star",
+	"Star",
+	"Star",
+	"Star",
+	"Plus",
+	"Plus",
+	"Plus",
+	"Plus",
+	"Plus",
+	"Squiggle",
+	"Squiggle",
+	"Squiggle",
+	"Squiggle",
+	"Squiggle",
+	
+}
+
+applyItemDetails.addDeck("ZenerCards", zenerCards)
