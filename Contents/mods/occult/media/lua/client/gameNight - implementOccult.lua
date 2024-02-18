e51a11486d438fe247bf339e6cae17ee35e02169
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-02-18 01:13:41 -0500

    Update gameNight - implementOccult.lua

--- Contents/mods/occult/media/lua/client/gameNight - implementOccult.lua
+++ Contents/mods/occult/media/lua/client/gameNight - implementOccult.lua
@@ -26,9 +26,9 @@ for _,s in pairs(playingCards.suits) do
 	end
 end
 
-applyItemDetails.addDeck("PlayingCards1", playingCards)
-applyItemDetails.addDeck("PlayingCards2", playingCards)
-applyItemDetails.addDeck("PlayingCards3", playingCards)
+applyItemDetails.addDeck("PlayingCards1", playingCards.cards)
+applyItemDetails.addDeck("PlayingCards2", playingCards.cards)
+applyItemDetails.addDeck("PlayingCards3", playingCards.cards)
 
 
 local prayerCards = {
