8baa1f4ee4918176ae1d85caaccb1482d160abab
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-04-05 15:29:20 -0400

    Update gameNight - DistributionsOccult.lua

--- Contents/mods/occult/media/lua/server/gameNight - DistributionsOccult.lua
+++ Contents/mods/occult/media/lua/server/gameNight - DistributionsOccult.lua
@@ -2,11 +2,46 @@ require "Items/SuburbsDistributions"
 
 local gameNightDistro = require "gameNight - Distributions"
 
-gameNightDistro.proceduralDistGameNight.itemsToAdd["TarotCards1"] = {chanceFactor = 0.5}
-gameNightDistro.proceduralDistGameNight.itemsToAdd["TarotCards2"] = {chanceFactor = 0.5}
-gameNightDistro.proceduralDistGameNight.itemsToAdd["ZenerCards"] = {chanceFactor = 0.3}
-gameNightDistro.proceduralDistGameNight.itemsToAdd["PrayerCards"] = {chanceFactor = 0.3}
-gameNightDistro.proceduralDistGameNight.itemsToAdd["OuijaBox"] = {chanceFactor = 0.75}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["TarotCards1"] = {
+    chanceFactor = 0.5,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.01,
+        ["ClassroomMisc"] = 0.01,
+        ["SchoolLockers"] = 0.01,
+    }
+}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["TarotCards2"] = {
+    chanceFactor = 0.5,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.01,
+        ["ClassroomMisc"] = 0.01,
+        ["SchoolLockers"] = 0.01,
+    }
+}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["ZenerCards"] = {
+    chanceFactor = 0.3,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.01,
+        ["ClassroomMisc"] = 0.01,
+        ["SchoolLockers"] = 0.01,
+    }
+}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["PrayerCards"] = {
+    chanceFactor = 0.3,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.01,
+        ["ClassroomMisc"] = 0.01,
+        ["SchoolLockers"] = 0.01,
+    }
+}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["OuijaBox"] = {
+    chanceFactor = 0.75,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.01,
+        ["ClassroomMisc"] = 0.01,
+        ["SchoolLockers"] = 0.01,
+    }
+}
 
 gameNightDistro.gameNightBoxes["OuijaBox"] = {
     rolls = 1,
